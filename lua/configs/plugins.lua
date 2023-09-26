d57b19341922fce5061e41c4586b37e3d264c85e
Axel Martin <martin.axel@sfr.fr>
2023-09-26 16:34:54 +0200

    update comment in plugins

--- lua/configs/plugins.lua
+++ lua/configs/plugins.lua
@@ -7,61 +7,68 @@ end
 vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use {
-    'svrana/neosolarized.nvim',
-    requires = { 'tjdevries/colorbuddy.nvim' }
-  }
-  use 'nvim-lualine/lualine.nvim'       -- Statusline
-  use 'nvim-lua/plenary.nvim'           -- Common utilities
-  use 'onsails/lspkind-nvim'            -- vscode-like pictograms
-  use 'hrsh7th/cmp-buffer'              -- nvim-cmp source for buffer words
-  use 'hrsh7th/cmp-nvim-lsp'            -- nvim-cmp source for neovim's built-in LSP
-  use 'hrsh7th/nvim-cmp'                -- Completion
-  use 'neovim/nvim-lspconfig'           -- LSP
-  use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+  use 'wbthomason/packer.nvim'    -- Packer can manage itself
+
+  use 'nvim-lualine/lualine.nvim' -- Statusline
+  use 'nvim-lua/plenary.nvim'     -- Common utilities
+
+  -- LSP support
+  use 'neovim/nvim-lspconfig' -- LSP
   use 'williamboman/mason.nvim'
   use 'williamboman/mason-lspconfig.nvim'
-  use 'MunifTanjim/prettier.nvim'
 
-  use 'nvimdev/lspsaga.nvim' -- LSP UIs
+  -- Autocompletion
+  use 'hrsh7th/nvim-cmp'       -- Completion
+  use 'hrsh7th/cmp-buffer'     -- nvim-cmp source for buffer words
+  use 'hrsh7th/cmp-nvim-lsp'   -- nvim-cmp source for neovim's built-in LSP
+  use 'windwp/nvim-autopairs'  -- check for pairs
+  use 'windwp/nvim-ts-autotag' -- auto close and auto rename tags
+  use 'Shougo/deoplete.nvim'   -- C / C++ autocompletion
+  use 'zchee/deoplete-clang'   -- C / C++ autocompletion
+
+  -- Snippets
+  use 'nvimdev/lspsaga.nvim'         -- LSP UIs
+  use 'onsails/lspkind-nvim'         -- vscode-like pictograms
   use 'L3MON4D3/LuaSnip'
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  }
   use 'kyazdani42/nvim-web-devicons' -- File icons
+
+  -- Git
+  use 'lewis6991/gitsigns.nvim'
+  use 'dinhhuy258/git.nvim' -- For git blame & browse
+
+  -- Fuzzy finder
   use 'nvim-telescope/telescope.nvim'
   use 'nvim-telescope/telescope-file-browser.nvim'
-  use 'windwp/nvim-autopairs'
-  use 'windwp/nvim-ts-autotag'
+
+  --  Highlight
+  use 'akinsho/nvim-bufferline.lua'
+  use 'norcalli/nvim-colorizer.lua'
+
+  -- Comment
   use { 'numToStr/Comment.nvim',
     requires = {
       'JoosepAlviste/nvim-ts-context-commentstring'
     }
   }
-  use 'norcalli/nvim-colorizer.lua'
-  use 'folke/zen-mode.nvim'
   use({
     "iamcco/markdown-preview.nvim",
     run = function() vim.fn["mkdp#util#install"]() end,
   })
-  use 'akinsho/nvim-bufferline.lua'
-  -- use 'github/copilot.vim'
 
-  use 'lewis6991/gitsigns.nvim'
-  use 'dinhhuy258/git.nvim' -- For git blame & browse
+  -- Formatting
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+  }
+  use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+  use 'MunifTanjim/prettier.nvim'
+  use 'folke/zen-mode.nvim'
 
-  -- theme
+  -- Theme
   use { "catppuccin/nvim", as = "catppuccin" }
 
-  -- nvim helper
-  use {
-    "m4xshen/hardtime.nvim",
-    requires = { 'MunifTanjim/nui.nvim', "nvim-lua/plenary.nvim" }
-  }
 
-  -- tmux helper
+  -- Tmux helper
   use { 'alexghergh/nvim-tmux-navigation', config = function()
     local nvim_tmux_nav = require('nvim-tmux-navigation')
 
@@ -78,9 +85,25 @@ packer.startup(function(use)
   end
   }
 
-  -- learning game, to remove
-  use 'ThePrimeagen/vim-be-good'
+  -- Nvim helpers
+  use 'ThePrimeagen/vim-be-good' -- Game to learn vim motions
+  use {
+    "m4xshen/hardtime.nvim",     -- Prohibits the use of mouse and force keymap usage
+    requires = { 'MunifTanjim/nui.nvim', "nvim-lua/plenary.nvim" }
+  }
 
-  --prisma helper
+  -- Prisma helper
   use 'prisma/vim-prisma'
+
+  -- Emmet
+  use 'mattn/emmet-vim'
+
+  -- Live server for html
+  use({
+    "aurum77/live-server.nvim",
+    run = function()
+      require "live_server.util".install()
+    end,
+    cmd = { "LiveServer", "LiveServerStart", "LiveServerStop" },
+  })
 end)
